f53d6888e72a124bd1172ec7f9df6ac728a5f98e
Marius Wichtner <mwichtner@coconut-capital.com>
2018-07-03 20:29:36 +0200

    refactored names of agent functions

--- src/td/agent.py
+++ src/td/agent.py
@@ -31,6 +31,31 @@ class Agent:
         self.V[state] = self.V[state] + self.alpha * td_delta
         self.N[state] += 1
 
+    def get_greedy_state_and_move(self, game, already_visited):
+        if (1 - self.epsilon) < np.random.rand():
+            return self._move_to_random_state(game)
+        else:
+            greedy_state = self.get_greedy_state(already_visited, game)
+            reward, done = game.move_to_state(greedy_state)
+            return greedy_state, reward, done
+
+    def get_greedy_state(self, already_visited, game):
+        valid_states = game.get_valid_states()
+        best_state = self.find_state_max_v(game, valid_states)
+        best_state = self.remove_states_visited_twice(already_visited, best_state, game, valid_states)
+        return best_state
+
+    def remove_states_visited_twice(self, already_visited, best_state, game, valid_states):
+        if already_visited[hash(best_state.tostring())] > 1:
+            valid_states = list(filter(lambda s: hash(s.tostring()) == hash(best_state.tostring()), valid_states))
+            best_state = self.find_state_max_v(game, valid_states)
+        return best_state
+
+    def find_state_max_v(self, game, valid_states):
+        v_states = self.get_v_states(valid_states, game)
+        best_state = valid_states[v_states.index(max(v_states))]
+        return best_state
+
     def get_v_states(self, valid_states, game):
         v_states = []
         for s in valid_states:
@@ -44,28 +69,10 @@ class Agent:
                 v_states.append(self.V[hash(s.tostring())])
         return v_states
 
-    def get_greedy_state_and_move(self, game: Game, all_states_list: dict, already_visited: dict):
-        self.epsilon *= self.epsilon_decay
-        if (1 - self.epsilon) < np.random.rand():  # get random state
-            return self._get_random_state(game)
-        else:
-            valid_states = game.get_valid_states()
-
-            v_states = self.get_v_states(valid_states, game)
-            best_state = valid_states[v_states.index(max(v_states))]
-
-            if already_visited[hash(best_state.tostring())] > 1:
-                valid_states = list(filter(lambda s: hash(s.tostring()) == hash(best_state.tostring()), valid_states))
-                v_states = self.get_v_states(valid_states, game)
-                best_state = valid_states[v_states.index(max(v_states))]
-
-            reward, done = game.move_to_state(best_state)
-            return best_state, reward, done, False
-
-    def _get_random_state(self, game: Game) -> (object, int, bool):
+    def _move_to_random_state(self, game: Game) -> (object, int, bool):
         action = self.get_random_action(game)
         state_before_random_move = game.get_field_state()
         reward, done, _ = game.move2(action)
         state = game.get_field_state()
         self.state_map[hash(state_before_random_move.tostring())].add(hash(state.tostring()))
-        return game.get_field_state(), reward, done, True
+        return game.get_field_state(), reward, done
--- src/td/td.py
+++ src/td/td.py
@@ -13,6 +13,8 @@ class Statistics:
     avg_reward: [float] = 0
     max_reward: [int] = 0
     td_delta: [float] = []
+    alpha: [float] = []
+    epsilon: [float] = []
 
 
 statistics = Statistics()
@@ -20,7 +22,7 @@ statistics = Statistics()
 all_collected_states = {}
 
 
-def td_learning(num_episodes, gamma=0.99, alpha=1.0, epsilon=0.5, epsilon_decay=0.009):
+def td_learning(num_episodes, gamma=0.99, alpha=1.0, epsilon=0.5, epsilon_decay=1.0):
     agent = Agent(gamma, alpha, epsilon, epsilon_decay)
 
     for i_episode in range(num_episodes):
@@ -31,14 +33,9 @@ def td_learning(num_episodes, gamma=0.99, alpha=1.0, epsilon=0.5, epsilon_decay=
         done = False
 
         visited_states = defaultdict(lambda: 0)
-        random_count = 0
         visited_states[hash(state.tostring())] += 1
         while not done:
-            next_state, reward, done, is_random = agent.get_greedy_state_and_move(game, all_collected_states,
-                                                                                  visited_states)
-            if is_random:
-                random_count += 1
-            # game.update_ui()
+            next_state, reward, done = agent.get_greedy_state_and_move(game, visited_states)
 
             next_state_hash = hash(next_state.tostring())
 
@@ -51,47 +48,51 @@ def td_learning(num_episodes, gamma=0.99, alpha=1.0, epsilon=0.5, epsilon_decay=
             agent.learn(next_state, reward, state)
             state = next_state
 
-            collect_stats(done, i_episode, num_episodes, total_reward)
+            collect_stats(done, i_episode, num_episodes, total_reward, agent.alpha, agent.epsilon)
 
-            if i_episode > num_episodes - 2: # print last episode
+            if i_episode > num_episodes - 2:  # print last episode
                 game.update_ui()
 
             if done:
-                # agent.epsilon = agent.epsilon - (1 / num_episodes)
+                agent.epsilon = agent.epsilon - (1 / num_episodes)
                 if i_episode > num_episodes - 100:
                     agent.epsilon = 0
-                print(f"episode: {i_episode} finished with reward: {total_reward}, random moves: {random_count}")
+                print(f"episode: {i_episode} finished with reward: {total_reward}")
     return agent
 
 
-def collect_stats(done, i_episode, num_episodes, total_reward):
+def collect_stats(done, i_episode, total_reward, td_delta, alpha, epsilon):
     if done:
         if total_reward > statistics.max_reward:
             statistics.max_reward = total_reward
-        if i_episode > num_episodes - 100:
-            statistics.avg_reward += total_reward
+        statistics.avg_reward += total_reward
         statistics.x.append(i_episode)
         statistics.y.append(total_reward)
+        statistics.td_delta.append(td_delta)
         statistics.mean_average.append(statistics.avg_reward / (i_episode + 1))
+        statistics.alpha.append(alpha)
+        statistics.epsilon.append(epsilon)
 
 
 def plot_data():
     plotly.tools.set_credentials_file(username='2mawi2', api_key='AylpnmyLc4ghzSemcCwM')
     xy_data = go.Scatter(x=statistics.x, y=statistics.y, mode='markers', marker=dict(size=4), name='reward')
     mov_avg = go.Scatter(x=statistics.x[5:-4], y=statistics.mean_average[5:-4],
-                         line=dict(width=2, color='red'), name='Moving average')
-    data = [xy_data, mov_avg]
-    try:
-        plotly.plotly.iplot(data, filename='results')
-    except:
-        pass
+                         line=dict(width=2, color='red'), name='Moving reward average')
+    epsilon = go.Scatter(x=statistics.x[5:-4], y=statistics.epsilon[5:-4],
+                         line=dict(width=2, color='blue'), name='epsilon')
+    alpha = go.Scatter(x=statistics.x[5:-4], y=statistics.alpha[5:-4],
+                       line=dict(width=2, color='blue'), name='alpha')
+
+    data = [xy_data, mov_avg, epsilon, alpha]
+
+    plotly.plotly.iplot(data, filename='results')
 
 
 if __name__ == '__main__':
     agent = td_learning(
-        num_episodes=5000,
+        num_episodes=1000,
         gamma=1,
-        epsilon=0.5,
-        epsilon_decay=1
+        epsilon=1,
     )
     plot_data()
